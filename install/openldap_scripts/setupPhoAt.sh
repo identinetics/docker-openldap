2837867cd59b69ad6da610de4b4b58bef5295948
war <thomas@warwaris.at>
2017-09-07 21:10:53 +0200

    restrict frontend

--- install/openldap_scripts/setupPhoAt.sh
+++ install/openldap_scripts/setupPhoAt.sh
@@ -11,21 +11,27 @@ ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif
 
-/opt/init/openldap/scripts/root2me.sh /opt/init/openldap/ldifs/phoat_manager.ldif
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/phoat_manager.ldif
+# configure the mdb
+ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/phoat_config.ldif
+
+# frontend access restriction
+ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/restrict_frontend.ldif
+
 
 # our additional schemas
 
 ldapadd -Y EXTERNAL -H ldapi:/// -f  /opt/init/openldap/schemas/phonlineperson.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/schemas/idnsyncstat.ldif
 
-# configure the mdb
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/phoat_config.ldif
-
 # init compare overlay
 
 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/twcompare_module.ldif
 ldapadd -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/phoat_twcompare_config.ldif
+
+# manager access
+
+/opt/init/openldap/scripts/root2me.sh /opt/init/openldap/ldifs/phoat_manager.ldif
+ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/init/openldap/ldifs/phoat_manager.ldif
 
 # PH Structure
 
